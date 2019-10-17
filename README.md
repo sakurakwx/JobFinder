cd0fb5daa03ebb90a66207679bccdcd216062925
AlexFanw <alex8802732@gmail.com>
2019-10-17 15:33:45 +0800

    2

---
+++ README.md
@@ -0,0 +1,6 @@
+# Alex Fan 's LeetCode Practice
+>1
+
+>2
+>> x = y if z else k    
+>> divmod()的使用
--- draft.py
+++ draft.py
@@ -12,3 +12,18 @@ class people:
 
 p = people('runoob',10,20)
 p.speak()
+
+class student(people):
+    grade = ''
+    def __init__(self,n,a,w,g):
+        #调用父类的构函
+        people.__init__(self,n,a,w)
+        self.grade = g
+    #覆写父类的方法
+    def speak(self):
+        print("%s 说: 我 %d 岁了，我在读 %d 年级"%(self.name,self.age,self.grade))
+ 
+ 
+ 
+s = student('ken',10,60,3)
+s.speak()
