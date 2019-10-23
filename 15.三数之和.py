3263ab9c85d84463dd81b68944c13be3a185ff6d
AlexFanw <alex8802732@gmail.com>
2019-10-23 17:13:01 +0800

    15.py

--- 15.三数之和.py
+++ 15.三数之和.py
@@ -5,8 +5,13 @@
 #
 
 # @lc code=start
+nums = [-1, 0, 1, 2, -1, -4]
+
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
+    def threeSum(self, nums):
+        print(nums)
+
+test = Solution()
+test.threeSum(nums)
 # @lc code=end
 
